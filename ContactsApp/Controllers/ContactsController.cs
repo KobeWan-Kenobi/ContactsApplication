faafda8d20494599eea18b45db909851df1ab941
KobeWan-Kenobi <76134958+KobeWan-Kenobi@users.noreply.github.com.>
2026-07-03 21:11:46 -0600

    completed ContactsViewModel class with constructors and methods to access repo

---
+++ ContactsApp/Controllers/ContactsController.cs
@@ -0,0 +1,16 @@
+﻿using Contacts.DataAccess.EF.Context;
+using ContactsApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsApp.Controllers
+{
+    public class ContactsController : Controller
+    {
+        private readonly ContactsContext _context;
+        public IActionResult Index()
+        {
+            ContactsViewModel model = new ContactsViewModel(_context);
+            return View(model);
+        }
+    }
+}
--- ContactsApp/ViewModels/ContactsViewModel.cs
+++ ContactsApp/ViewModels/ContactsViewModel.cs
@@ -1,15 +1,16 @@
 ﻿using Contacts.DataAccess.EF.Repositories;
 using Contacts.DataAccess.EF.Models;
 using Contacts.DataAccess.EF.Context;
+using System.ComponentModel;
 namespace ContactsApp.Models
 {
     public class ContactsViewModel
     {
         private ContactRepository _repo;
         public List<Contact>? ContactList { get; set; }
-        public Contact? CurrentContact {  get; set; }
-        public bool IsActionSuccess {  get; set; }
-        public string ActionMessage {  get; set; }
+        public Contact? CurrentContact { get; set; }
+        public bool? IsActionSuccess { get; set; }
+        public string? ActionMessage { get; set; }
         public ContactsViewModel(ContactsContext context)
         {
             _repo = new ContactRepository(context);
@@ -20,11 +21,32 @@ namespace ContactsApp.Models
         {
             _repo = new ContactRepository(context);
             ContactList = GetAllContacts();
-            if(contactID > 0)
+            if (contactID > 0)
             {
                 CurrentContact = GetContact(contactID);
 
             }
+        }
+        public void SaveContact(Contact contact)
+        {
+            if (contact.ContactId > 0)
+            {
+                _repo.Update(contact);
+            }
+            else
+            {
+                contact.ContactId = _repo.Create(contact);
+            }
+            ContactList = GetAllContacts();
+            CurrentContact = GetContact(contact.ContactId);
+        }
+        public List<Contact> GetAllContacts()
+        {
+            return _repo.GetAllContacts();
+        }
+        public Contact GetContact(int contactID)
+        {
+            return _repo.GetContactById(contactID);
         }
     }
 }
