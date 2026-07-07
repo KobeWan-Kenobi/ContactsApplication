4cbb6bdd3c091cc88acba5e4956ee77f1f389e8e
KobeWan-Kenobi <76134958+KobeWan-Kenobi@users.noreply.github.com.>
2026-07-06 18:27:19 -0600

    Created constructors for Contact model class

--- Contacts.DataAccess.EF/Contacts.DataAccess.EF/Models/Contact.cs
+++ Contacts.DataAccess.EF/Contacts.DataAccess.EF/Models/Contact.cs
@@ -1,17 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Contacts.DataAccess.EF.Models;
-
-public partial class Contact
+namespace Contacts.DataAccess.EF.Models
 {
-    public int ContactId { get; set; }
 
-    public string? FullName { get; set; }
+    public partial class Contact
+    {
+        public int ContactId { get; set; }
 
-    public string? Phone { get; set; }
+        public string? FullName { get; set; }
 
-    public string? Email { get; set; }
+        public string? Phone { get; set; }
 
-    public bool? IsFavorite { get; set; }
+        public string? Email { get; set; }
+
+        public bool? IsFavorite { get; set; }
+
+        public Contact(int contactId, string fullName, string phone, string email, bool isFavorite)
+        {
+            ContactId = contactId;
+            FullName = fullName;
+            Phone = phone;
+            Email = email;
+            IsFavorite = isFavorite;
+        }
+        public Contact()
+        {
+
+        }
+    }
 }
