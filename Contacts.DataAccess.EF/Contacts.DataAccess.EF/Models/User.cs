11d04d703174558c15206d5bab09c08c81037b5c
KobeWan-Kenobi <76134958+KobeWan-Kenobi@users.noreply.github.com.>
2026-07-08 19:38:05 -0600

    Created new User model file

---
+++ Contacts.DataAccess.EF/Contacts.DataAccess.EF/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+
+namespace Contacts.DataAccess.EF.Models
+{
+    public class User : IdentityUser
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
