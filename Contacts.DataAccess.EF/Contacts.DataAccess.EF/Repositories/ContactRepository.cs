71598605709a6c9ce98933b8d775e3f295bd67f0
KobeWan-Kenobi <76134958+KobeWan-Kenobi@users.noreply.github.com.>
2026-07-09 23:40:58 -0600

    Changed to contactsDbContext

--- Contacts.DataAccess.EF/Contacts.DataAccess.EF/Repositories/ContactRepository.cs
+++ Contacts.DataAccess.EF/Contacts.DataAccess.EF/Repositories/ContactRepository.cs
@@ -10,49 +10,49 @@ namespace Contacts.DataAccess.EF.Repositories
 
     public class ContactRepository : IContactRepository
     {
-        private ContactsDbContext _contactsContext;
+        private ContactsDbContext _contactsDbContext;
         public ContactRepository(ContactsDbContext contactsContext)
         {
-            _contactsContext = contactsContext;
+            _contactsDbContext = contactsContext;
         }
         public int Create(Contact contact)
         {
-            _contactsContext.Add(contact);
-            _contactsContext.SaveChanges();
+            _contactsDbContext.Add(contact);
+            _contactsDbContext.SaveChanges();
             return contact.ContactId;
         }
         public int Update(Contact contact)
         {
-            Contact? existingContact = _contactsContext.Contacts.Find(contact.ContactId);
+            Contact? existingContact = _contactsDbContext.Contacts.Find(contact.ContactId);
             if (existingContact != null)
             {
                 existingContact.FullName = contact.FullName;
                 existingContact.Phone = contact.Phone;
                 existingContact.Email = contact.Email;
                 existingContact.IsFavorite = contact.IsFavorite;
-                _contactsContext.SaveChanges();
+                _contactsDbContext.SaveChanges();
             }
             return contact.ContactId;
         }
         public bool Delete(int contactID)
         {
-            Contact? oldContact = _contactsContext.Contacts.Find(contactID);
+            Contact? oldContact = _contactsDbContext.Contacts.Find(contactID);
             if (oldContact != null)
             {
-                _contactsContext.Contacts.Remove(oldContact);
-                _contactsContext.SaveChanges();
+                _contactsDbContext.Contacts.Remove(oldContact);
+                _contactsDbContext.SaveChanges();
                 return true;
             }
             return false;
         }
         public List<Contact> GetAllContacts()
         {
-            return _contactsContext.Contacts.ToList();
+            return _contactsDbContext.Contacts.ToList();
         }
 
         public Contact GetContactById(int contactID)
         {
-            Contact? contact = _contactsContext.Contacts.Find(contactID);
+            Contact? contact = _contactsDbContext.Contacts.Find(contactID);
             return contact;
         }
     }
