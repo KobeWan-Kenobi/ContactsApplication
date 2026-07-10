10bdbb8f902c4d678c7a4b38f73003e3908117a1
KobeWan-Kenobi <76134958+KobeWan-Kenobi@users.noreply.github.com.>
2026-07-09 23:42:22 -0600

    Migrated the contactId Primary key

---
+++ Contacts.DataAccess.EF/Contacts.DataAccess.EF/Migrations/20260710041336_AddContactPrimaryKey.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Contacts.DataAccess.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddContactPrimaryKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
