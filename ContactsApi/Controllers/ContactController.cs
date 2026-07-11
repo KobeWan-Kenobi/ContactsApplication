e4c64381911a681359f5c0dc6e98f34ca122b647
KobeWan-Kenobi <76134958+KobeWan-Kenobi@users.noreply.github.com.>
2026-07-10 20:49:27 -0600

    Created ContactCreateDTO and implemented endpoint for CreateContact in controller

--- ContactsApi/Controllers/ContactController.cs
+++ ContactsApi/Controllers/ContactController.cs
@@ -3,6 +3,7 @@ using ContactsApp.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Contacts.DataAccess.EF.Models;
 using System.Net;
+using ContactsApi.Models.DTOs;
 
 namespace ContactsApp.Controllers
 {
@@ -26,11 +27,11 @@ namespace ContactsApp.Controllers
             return Ok(_response);
         }
 
-        [HttpGet("{contactId:int}", Name="GetContact")]
+        [HttpGet("{contactId:int}", Name = "GetContact")]
         public IActionResult GetContact(int contactId)
         {
             ContactViewModel viewModel = new ContactViewModel(_context);
-            if(contactId == 0)
+            if (contactId == 0)
             {
                 _response.StatusCode = HttpStatusCode.BadRequest;
                 _response.IsSuccess = false;
@@ -40,6 +41,51 @@ namespace ContactsApp.Controllers
             _response.StatusCode = HttpStatusCode.OK;
             return Ok(_response);
         }
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse>> CreateContact([FromBody] ContactCreateDTO contactCreateDTO)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.ErrorMessages = ["Error: ModelState is invalid"];
+                    return BadRequest(_response);
+                }
+                else if(contactCreateDTO == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.ErrorMessages = ["Error: contactCreateDTO is null"];
+                    return BadRequest(_response);
+                }
+                else
+                {
+                    ContactViewModel viewModel = new ContactViewModel(_context);
+                    Contact newContact = new()
+                    {
+                        FullName = contactCreateDTO.FullName,
+                        Phone = contactCreateDTO.Phone,
+                        Email = contactCreateDTO.Email,
+                        IsFavorite = contactCreateDTO.IsFavorite,
+                    };
+                    viewModel.SaveContact(newContact);
+                    _response.IsSuccess = true;
+                    _response.Result = contactCreateDTO;
+                    _response.StatusCode = HttpStatusCode.Created;
+                    return CreatedAtRoute("GetContact", new { contactId = newContact.ContactId }, _response);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = [ex.ToString()];
+            }
+            return BadRequest(_response);
+        }
+
 
         public IActionResult Update(int contactId)
         {
--- ContactsApi/Models/DTOs/ContactCreateDTO.cs
+++ ContactsApi/Models/DTOs/ContactCreateDTO.cs
@@ -1,6 +1,16 @@
-﻿namespace ContactsApi.Models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactsApi.Models.DTOs
 {
     public class ContactCreateDTO
     {
+        [Required]
+        public string FullName { get; set; } = string.Empty;
+
+        public string Phone { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public bool IsFavorite { get; set; } = false;
     }
 }
