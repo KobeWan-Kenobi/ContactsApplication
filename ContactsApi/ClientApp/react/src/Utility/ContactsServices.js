b3f2f0447f630bdefc9a4ce601a6c52151cb05bf
KobeWan-Kenobi <76134958+KobeWan-Kenobi@users.noreply.github.com.>
2026-07-13 17:20:07 -0600

    Created new file for making api calls

--- ContactsApi/ClientApp/react/src/Utility/ContactsServices.js
+++ ContactsApi/ClientApp/react/src/Utility/ContactsServices.js
@@ -1,8 +1,20 @@
+import axios from "axios";
 const BASE_URL = "https://localhost:44341/api/Contacts";
 const getAllContacts = async () => {
-  const url = new URL("https://localhost:44341/api/Contacts");
+  const url = new URL(BASE_URL);
 
-  const response = await axios.get(url);
-  return response.data;
+
+    const response = await axios.get(url).then(data=>{
+      return data;
+    })
+    .catch((error)=>{
+      console.log("Error", error);
+      return -1;
+    });
+    return response.data.result.map(contact => {
+      const {fullName, ...rest} = contact;
+      return {...rest, name: fullName};
+    })
+
 };
 export default getAllContacts;
