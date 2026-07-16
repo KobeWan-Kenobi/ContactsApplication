b96d9c2bffca3a07c1471f0fcf66dc9aced5c0ff
KobeWan-Kenobi <76134958+KobeWan-Kenobi@users.noreply.github.com.>
2026-07-16 00:38:00 -0600

    normalized response with ContactDTO type and added remaining CRUD methods

--- ContactsApi/ClientApp/react/src/Utility/ContactsServices.ts
+++ ContactsApi/ClientApp/react/src/Utility/ContactsServices.ts
@@ -1,26 +1,68 @@
 import axios from "axios";
-import {ContactDTO} from "./DTOs/ContactDTO";
+import { ContactDTO } from "./DTOs/ContactDTO";
 import { ContactsApiResponse } from "./Models/ContactsApiResponse";
 import { Contact } from "./Models/Contact";
+import { ContactCreateDTO } from "./DTOs/ContactCreateDTO";
 const BASE_URL = "https://localhost:44341/api/Contacts";
-const getAllContacts = async (): Promise<Contact[]> => {
+export const getAllContacts = async (): Promise<Contact[]> => {
   try {
     const response = await axios.get<ContactsApiResponse>(BASE_URL);
     //backend is contactId while frontend uses id
     // backend is fullName while frontend uses name
-    return response.data.result.map((contact: ContactDTO): Contact => {
-      return {
-        id: contact.contactId,
-        name: contact.fullName,
-        phone: contact.phone,
-        email: contact.email,
-        isFavorite: contact.isFavorite,
-      };
-    });
+    return response.data.result.map(
+      (contactDTO: ContactDTO): Contact => {
+        return {
+          id: contactDTO.contactId,
+          name: contactDTO.fullName,
+          phone: contactDTO.phone,
+          email: contactDTO.email,
+          isFavorite: contactDTO.isFavorite,
+        };
+      },
+    );
   } catch (error) {
     console.log("Error", error);
     return [];
   }
 };
+export const createContact = async (contactCreateDTO: ContactCreateDTO): Promise<number> => {
+  try {
 
-export default getAllContacts;
+    const response = await axios.post<ContactsApiResponse>(
+      BASE_URL,
+      contactCreateDTO,
+    );
+    return response.data.result.contactId;
+
+  } catch (error) {
+    console.log("Error", error);
+    return -1;
+  }
+};
+export const updateContact = async(contactDTO: ContactDTO): Promise<ContactsApiResponse> => {
+  try {
+
+    const response = await axios.put<ContactsApiResponse>(
+      BASE_URL,
+      contactDTO,
+    );
+    return response.data;
+
+  } catch (error) {
+    console.log("Error", error);
+    const badResponse: ContactsApiResponse = {
+      statusCode: 400,
+      result: null,
+      errorMessages: [`An error was encountered before the server could be reached: ${error}`],
+      isSuccess: false
+    }
+    return badResponse;
+  }
+}
+export const deleteContact = async(contactId: number): Promise<boolean> =>{
+  const response = await axios.delete<ContactsApiResponse>(
+    `${BASE_URL}/${contactId}`
+  );
+  return response.data.isSuccess;
+
+}
